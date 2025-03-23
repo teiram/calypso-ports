f5a68a035897649ebbf88024be6ce425f70ea96b
Manuel Teira <manuel.teira@gmail.com>
2025-03-23 16:03:17 +0100

    svi328: Remove unused CE

--- svi328/SVI328.sv
+++ svi328/SVI328.sv
@@ -149,14 +149,12 @@ pll pll(
 
 reg ce_10m7 = 0;
 reg ce_5m3 = 0;
-reg ce_21m3 = 0;
 always @(posedge clk_sys) begin
     reg [2:0] div;
 
     div <= div + 1'd1;
     ce_10m7 <= !div[1:0];
     ce_5m3  <= !div[2:0];
-    ce_21m3 <= div[0];
 end
 
 wire [31:0] status;
