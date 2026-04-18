9d8f3228f33d34d0b594178dead3ca891a64d3f3
Manuel Teira <manuel.teira@gmail.com>
2026-04-18 20:52:03 +0200

    build_id for vhdl

---
+++ common/build_id_vhdl.tcl
@@ -0,0 +1,41 @@
+proc generateBuildID_VHDL {} {
+
+	set buildDate [ clock format [ clock seconds ] -format %y%m%d ]
+	set buildTime [ clock format [ clock seconds ] -format %H%M%S ]
+
+	set coreDefinitionFileName "core.definition"
+	set version {}
+	if { ![catch {open $coreDefinitionFileName r} coreDefinitionFile] } {
+		fconfigure $coreDefinitionFile -buffering line
+		gets $coreDefinitionFile data
+		while {$data != ""} {
+     		gets $coreDefinitionFile data
+     		if { [regexp {VERSION=(.*)} "$data" all version] == 1 } {
+     			break
+     		}
+		}
+		close $coreDefinitionFile
+	}
+	
+
+	# Create a Verilog file for output
+	set outputFileName "build_id.vhdl"
+	set outputFile [open $outputFileName "w"]
+
+	# Output the VHDL source
+	puts $outputFile "package calypso_version is"
+	puts $outputFile "    constant BUILD_DATE: string := \"$buildDate\";"
+	puts $outputFile "    constant BUILD_TIME: string := \"$buildTime\";"
+	puts $outputFile "    constant BUILD_VERSION: string := \"calypso-$version\";"
+	puts $outputFile "end package;"
+	close $outputFile
+
+	# Send confirmation message to the Messages window
+	post_message "Generated build identification VHDL module: [pwd]/$outputFileName"
+	post_message "Date:             $buildDate"
+	post_message "Time:             $buildTime"
+	post_message "Version:          $version"
+}
+
+# Comment out this line to prevent the process from automatically executing when the file is sourced:
+generateBuildID_VHDL
